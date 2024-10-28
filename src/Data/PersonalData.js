b2dd88b6c4f174b05663f180d7d3a2b190a979d7
AmaanIshrat <ishratamaan@gmail.com>
2024-10-28 17:07:32 +0530

    abtme

--- src/Data/PersonalData.js
+++ src/Data/PersonalData.js
@@ -5,7 +5,7 @@ const PersonalData = {
     mobNo:"+91 7619020078",
     email:"ishratamaan@gmail.com",
     address:"India",
-    aboutMe:"Hi Everyone, I am Amaan Ishrat from Uttar Pradesh, India.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, NodeJS. Have a keen knowledge of databases like MySQL, MongoDB and Firebase. Eager to contribute to innovative projects and gain hands-on experience in a dynamic and collaborative",
+    aboutMe:"Hi Everyone, I am Amaan Ishrat from Uttar Pradesh, India.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, NodeJS. Have a keen knowledge of databases like MySQL. Eager to contribute to innovative projects and gain hands-on experience in a dynamic and collaborative",
     resumeLink:'https://drive.google.com/file/d/1cEmH6koI4d2x1dP334CtpART4ZroU6Zy/view?usp=sharing',
     imageUrl:''
 }
